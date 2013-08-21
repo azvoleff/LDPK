bf53c6dc0f9fec4e45c56fdbe9065c296192a6c4
Alex Zvoleff <azvoleff@outlook.com>
2013-08-21 12:49:13 -0400

    Add error handling and better status tracking.

--- 6_missing_data_stats.R
+++ 6_missing_data_stats.R
@@ -16,39 +16,48 @@ mask_ZOI <- function(img, zoi, bufferwidth=1000) {
 
 band_names <- list('band1', 'band2', 'band3', 'band4', 'band5', 'band6', 
                    'band7')
-band_names <- list('band1')
 
 image_list$pct_cloud <- NA
 image_list$pct_missing <- NA
 
-#for (n in 1:nrow(image_list)) {
-for (n in 1:1) {
-    base_in_file <- sub('.hdf$', '', image_list[n, ]$file_path)
-    base_out_file <- sub('orig', 'proc', base_in_file)
+for (n in 1:nrow(image_list)) {
+    in_file_prefix <- sub('.hdf$', '', image_list[n, ]$file_path)
+    out_file_prefix <- sub('orig', 'proc', in_file_prefix)
+    print(paste('Processing', in_file_prefix))
 
     for (band in band_names) {
-        b <- raster(paste(base_in_file, '_', band, '.bsq', sep=''))
+        b_out_file_prefix <- paste(out_file_prefix, band, 'zoi_crop', sep='_')
+        if (file.exists(paste(b_out_file_prefix, '.envi', sep=''))) {
+            print(paste('skipping', band, '- file already exists'))
+            next
+        }
+        print(paste('writing', band))
+        b <- raster(paste(in_file_prefix, '_', band, '.bsq', sep=''))
         b_masked <- mask_ZOI(b, zoi)
         writeRaster(b_masked,
-                    paste(base_out_file, band, 'zoi_crop', sep='_'), 
+                    paste(out_file_prefix, band, 'zoi_crop', sep='_'), 
                     format='ENVI')
     }
 
-    fill_QA <- raster(paste(base_in_file, '_fill_QA.bsq', sep=''))
+    print(paste('writing', 'fill_QA'))
+    fill_QA <- raster(paste(in_file_prefix, '_fill_QA.bsq', sep=''))
     fill_QA_masked <- mask_ZOI(fill_QA, zoi)
     writeRaster(fill_QA_masked,
-                paste(base_out_file, '_fill_QA_crop', sep=''), format='ENVI')
+                paste(out_file_prefix, '_fill_QA_crop', sep=''), format='ENVI')
     fill_QA_masked_stats <- table(getValues(fill_QA_masked))
     image_list$pct_missing[n] <- 100 - as.numeric(fill_QA_masked_stats['0'] / 
                                                   sum(fill_QA_masked_stats) * 
                                                   100)
      
-    cloud <- raster(paste(base_out_file, '_comb_cloud_mask.bsq', sep=''))
+    print(paste('writing', 'comb_cloud_mask'))
+    cloud <- raster(paste(out_file_prefix, '_comb_cloud_mask.bsq', sep=''))
     cloud_crop_masked <- mask_ZOI(cloud, zoi)
     writeRaster(cloud_crop_masked,
-                paste(base_out_file, '_comb_cloud_mask_crop', sep=''), 
+                paste(out_file_prefix, '_comb_cloud_mask_crop', sep=''), 
                 format='ENVI')
     cloud_crop_masked_stats <- table(getValues(cloud_crop_masked))
     image_list$pct_cloud[n] <- as.numeric(cloud_crop_masked_stats['255'] / 
                                           sum(cloud_crop_masked_stats) * 100)
 }
+
+write.csv(image_list, file='H:/Data/TEAM/VB/Rasters/Landsat/image_list.csv')
