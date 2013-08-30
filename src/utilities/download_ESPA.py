e90218a13b471a111d046df2df665becab719944
Alex Zvoleff <azvoleff@conservation.org>
2013-08-30 13:53:36 -0400

    Verify checksum on downloaded files.

--- src/utilities/download_ESPA.py
+++ src/utilities/download_ESPA.py
@@ -7,6 +7,7 @@ import os
 import sys
 import re
 import argparse # Requires Python 2.7 or above
+import hashlib
 import urllib
 
 def main():
@@ -53,17 +54,63 @@ def main():
     # Progress bar code is from: http://bit.ly/1a5JW7H
     global progress_text # global variable to be used in dlProgress
     def dlProgress(count, blockSize, totalSize):
-          percent = int(count * blockSize * 100 / totalSize)
-          sys.stdout.write("\r" + progress_text + "...%d%%"%percent)
-          sys.stdout.flush()
-    
+        global progress_text # global variable to be used in dlProgress
+        percent = int(count * blockSize * 100 / totalSize)
+        sys.stdout.write("\r" + progress_text + "...%d%%"%percent)
+        sys.stdout.flush()
+
+    def get_md5_for_file(file, block_size=2**20):
+        f = open(file, 'rb')
+        md5 = hashlib.md5()
+        while True:
+            data = f.read(block_size)
+            if not data:
+                break
+            md5.update(data)
+        f.close()
+        return md5.hexdigest()
+
+    def check_ESPA_checksum(remote_url, local_url):
+        f = urllib.urlopen(re.sub('\.tar\.gz$', '.cksum', remote_url))
+        espa_checksum_file = f.read()
+        f.close()
+        espa_checksum_file = espa_checksum_file.split()
+        # TODO: Figure out WHAT checksum ESPA is using...
+        #if espa_checksum_file[0] != get_md5_for_file(local_url):
+        #    return 1
+        if int(espa_checksum_file[1]) != os.path.getsize(local_url):
+            return 1
+        else:
+            return 0
+
+    def download_ESPA_file(url, output_path):
+        global progress_text # global variable to be used in dlProgress
+        progress_text = 'File %s of %s: %s'%(n + 1, len(urls), filename)
+        urllib.urlretrieve(url, output_path, reporthook=dlProgress)
+        return check_ESPA_checksum(url, output_path)
+
+    successes = 0
+    failures = 0
+    skips = 0
     for n in xrange(len(urls)):
         url = urls[n]
         filename = os.path.basename(url)
-        progress_text = 'File %s of %s: %s'%(n + 1, len(urls), filename)
-        urllib.urlretrieve(url, os.path.join(output_folder, filename), reporthook=dlProgress)
+        output_path = os.path.join(output_folder, filename)
+        if os.path.exists(output_path):
+            if check_ESPA_checksum(url, output_path):
+                print 'Warning: %s exists but has bad checksum - re-downloading file'%output_path
+            else:
+                print 'Warning: %s exists and has good checksum - skipping download'%output_path
+                skips += 1
+                continue
+        if download_ESPA_file(url, output_path) == 0:
+            successes += 1
+        else:
+            print 'Warning: %s downloaded with bad checksum'%output_path
+            failures += 1
+        print
 
-    print 'Completed %s files'%len(urls)
-    
+    print '%s files succeeded. %s files skipped. %s files failed.'%(successes, skips, failures)
+
 if __name__ == "__main__":
     sys.exit(main())
