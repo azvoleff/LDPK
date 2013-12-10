33f31351083781813d5ffaf6ca8e269fd5ecd280
Alex Zvoleff <azvoleff@outlook.com>
2013-12-10 09:59:47 -0500

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ distribute_setup.use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-    name = "LDPK",
+    name = "teampy",
     version = "0.1",
-    packages = ['LDPK'],
-    package_dir = {'LDPK' : 'src'},
-    entry_points = {'console_scripts': ['LDPK_preprocess = LDPK.preprocess_image.py:main',
-                                        'LDPK_classify = LDPK.classify_image:main',
-                                        'LDPK_analyze = LDPK.analyze_classification:main']},
+    packages = ['teampy'],
+    package_dir = {'teampy' : 'src'},
+    entry_points = {'console_scripts': ['teampy_preprocess = teampy.preprocess_image.py:main',
+                                        'teampy_classify = teampy.classify_image:main',
+                                        'teampy_analyze = teampy.analyze_classification:main']},
     zip_safe = True,
     install_requires = ['numpy >= 1.7.0',
                         'matplotlib >= 0.98.4',
                         'gdal >= 1.7'],
     author = "Alex Zvoleff",
     author_email = "azvoleff@conservation.org",
-    description = "TEAM Landsat Data Processing Kit",
-    keywords = "Landsat remote-sensing imagery classification preprocessing land use land cover",
+    description = "TEAM Data Processing Tools (Python)",
+    keywords = "remote-sensing imagery classification preprocessing land use land cover Landsat",
     license = "GPL v3 or later",
-    url = "https://github.com/azvoleff/TEAM_Landat",   # project home page, if any
+    url = "https://github.com/azvoleff/teampy",   # project home page, if any
     long_description = ''.join(open('README.rst').readlines()[6:]),
     classifiers = [
         "Development Status :: 3 - Alpha",
